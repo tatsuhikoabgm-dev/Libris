3d036e7b7bb25aec9a6fb7331467bb2f4c392f3e
tatsuhikoabgm-dev <tatsuhiko.a.bgm.gmail.com>
2025-11-27 14:14:59 +0900

    初めの一歩
    GitHubで作成したドキュメントなどにSpringのプロジェクトを統合する

---
+++ src/main/java/com/tsd/libris/LibrisApplication.java
@@ -0,0 +1,13 @@
+package com.tsd.libris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsd/libris/LibrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsd.libris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
