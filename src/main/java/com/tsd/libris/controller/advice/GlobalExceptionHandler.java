6d7c3465f6361853256ec433475ac6bf7ef74866
tatsuhikoabgm-dev <tatsuhiko.a.bgm.gmail.com>
2025-12-23 15:23:39 +0900

    ドキュメントとかあげた
    ER図とかディレクトリ構造とかそこらへんあげたよん

--- src/main/java/com/tsd/libris/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/tsd/libris/controller/advice/GlobalExceptionHandler.java
@@ -12,37 +12,33 @@ import com.tsd.libris.exception.ApplicationException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	
-	@ExceptionHandler({MethodArgumentTypeMismatchException.class,
-						BindException.class,
-						IllegalArgumentException.class})
+	@ExceptionHandler({ MethodArgumentTypeMismatchException.class,
+			BindException.class,
+			IllegalArgumentException.class })
 	public String handleRequestBindException(Model model) {
-		
-		model.addAttribute("errorMessage","不正な値です！！変なことしないでください！ｗ");
+
+		model.addAttribute("errorMessage", "不正な値です！！変なことしないでください！ｗ");
 		return "error/common";
 	}
-	
-	
+
 	@ExceptionHandler(ApplicationException.class)
 	public String handleApplicationException(ApplicationException e,
-												Model model) {
-		
-		if(e.getType() == ApplicationException.Type.INVALID_REQUEST) {
-			model.addAttribute("errorMessage","不正な値です！！変なことしないでください！ｗ");
-		}else {
-			model.addAttribute("errorMessage","システムエラーです。もう一度試してみてください！");
+			Model model) {
+
+		if (e.getType() == ApplicationException.Type.INVALID_REQUEST) {
+			model.addAttribute("errorMessage", "不正な値です！！変なことしないでください！ｗ");
+		} else {
+			model.addAttribute("errorMessage", "システムエラーです。もう一度試してみてください！");
 		}
-		
-		
+
 		return "error/common";
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	public String handleOtherException(Model model) {
-		
-		model.addAttribute("errorMessage","不正な値です！！変なことしないでください！ｗ");
+
+		model.addAttribute("errorMessage", "不正な値です！！変なことしないでください！ｗ");
 		return "error/common";
 	}
-	
-	
+
 }
